bbf1a6fb4d6cc814e16523c21f7b252bfe1d3f95
gagva <goderdzi.valishvili.1@btu.edu.ge>
2024-03-06 20:38:32 +0400

    I have changed products design and made it scrollable

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,23 +4,25 @@ import "../styles/Products.scss";
 const Products = () => {
   return (
     <div className="produts-container">
+      <button id="left-arrow"><img src="../img/left-arrow.png" width="30px" /></button>
+      <div className="products-frame">
       <div className="first-row">
         <div className="first-item">
-          <img src="../img/search-icon.png" width="60px" />
+          <img src="../img/search-icon.png" width="100px" />
           <button>
             <img />
             Add To Cart
           </button>
         </div>
         <div className="second-item">
-          <img src="../img/search-icon.png" width="60px" />
+          <img src="../img/search-icon.png" width="100px" />
           <button>
             <img />
             Add To Cart
           </button>
         </div>
         <div className="third-item">
-          <img src="../img/search-icon.png" width="60px" />
+          <img src="../img/search-icon.png" width="100px" />
           <button>
             <img />
             Add To Cart
@@ -29,21 +31,21 @@ const Products = () => {
       </div>
       <div className="second-row">
         <div className="first-item">
-          <img src="../img/search-icon.png" width="60px" />
+          <img src="../img/search-icon.png" width="100px" />
           <button>
             <img />
             Add To Cart
           </button>
         </div>
         <div className="second-item">
-          <img src="../img/search-icon.png" width="60px" />
+          <img src="../img/search-icon.png" width="100px" />
           <button>
             <img />
             Add To Cart
           </button>
         </div>
         <div className="third-item">
-          <img src="../img/search-icon.png" width="60px" />
+          <img src="../img/search-icon.png" width="100px" />
           <button>
             <img />
             Add To Cart
@@ -52,27 +54,29 @@ const Products = () => {
       </div>
       <div className="third-row">
         <div className="first-item">
-          <img src="../img/search-icon.png" width="60px" />
+          <img src="../img/search-icon.png" width="100px" />
           <button>
             <img />
             Add To Cart
           </button>
         </div>
         <div className="second-item">
-          <img src="../img/search-icon.png" width="60px" />
+          <img src="../img/search-icon.png" width="100px" />
           <button>
             <img />
             Add To Cart
           </button>
         </div>
         <div className="third-item">
-          <img src="../img/search-icon.png" width="60px" />
+          <img src="../img/search-icon.png" width="100px" />
           <button>
             <img />
             Add To Cart
           </button>
         </div>
       </div>
+      </div>
+      <button id="right-arrow"><img src="../img/right-arrow.webp" width="30px" /></button>
     </div>
   );
 };
